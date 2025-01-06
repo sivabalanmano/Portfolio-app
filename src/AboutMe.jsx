6154c4ee408f1efe32e36e5eea6c6e8c3fd4ef11
sivabalanmano <siva08082001@gmail.com>
2025-01-06 06:28:02 +0530

    fix it

--- src/AboutMe.jsx
+++ src/AboutMe.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 
-const AboutMe = ({ref}) => {
+const AboutMe = () => {
 
   return (
     <div  className=" w-[100%] bg-gray-400">
         <div className="font-bold text-2xl justify-center items-center flex mt-4 mb-4 border-red-500 underline decoration-sky-500">
             AboutMe
         </div>
-      <div ref={ref}  className="items-center justify-center flex font-bold text-xl text-start  p-3 tracking-wide space-y-3 lg:p-36">
+      <div  className="items-center justify-center flex font-bold text-xl text-start  p-3 tracking-wide space-y-3 lg:p-36">
         <div>
         Hello! I'm Sivabaln, a passionate Front-End Developer and PL/SQL
         Developer with a B-Tech in Information Technology. With a keen interest
--- src/App.js
+++ src/App.js
@@ -4,26 +4,14 @@ import MinContent from './MinContent';
 import NavBar from './NavBar'
 import Projects from './Projects';
 import Skils from './Skils';
-import { useEffect, useRef } from 'react';
-import gsap from 'gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
-gsap.registerPlugin(ScrollTrigger);
 
 function App() {
-  const ref = useRef();
-    useEffect(() => {
-        const el = ref.current;
-        gsap.fromTo(el, { scale: 0 }, {
-            scale: 1, duration: 1, scrollTrigger: {
-                trigger: el
-            }
-        })
-    }, [])
+  
   return (
    <div>
     <NavBar />
     <MinContent />
-    <AboutMe ref={ref}/>
+    <AboutMe />
     <Skils />
     <Projects />
     <GetInTouch />
